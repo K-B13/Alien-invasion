f124bb0d1dacebe2618a99345efdeaf405257637
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-11-15 18:06:42 +0000

    Add the ability to fire bullets with space bar

--- bullet.py
+++ bullet.py
@@ -6,7 +6,7 @@ class Bullet(Sprite):
 
     def __init__(self, ai_game):
         '''Create a bullet object at the ships's cutrrent position.'''
-        super.__init__()
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
@@ -18,13 +18,13 @@ class Bullet(Sprite):
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
 
-        def update(self):
-            '''Move the bullet up the screen.'''
-            # Update the decimal position of the bullet.
-            self.y -= self.settings.bullet_speed
-            # Update the rect position.
-            self.rect.y = self.y
+    def update(self):
+        '''Move the bullet up the screen.'''
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
 
-            def draw_bullet(self):
-                '''Draw the bullet to the screen.'''
-                pygame.draw.rect(self.screen, self.color, self.rect)
+    def draw_bullet(self):
+        '''Draw the bullet to the screen.'''
+        pygame.draw.rect(self.screen, self.color, self.rect)
