c4e45c2ae2d8520ca2732e54172b8f6f633a69e0
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-11-17 10:58:26 +0000

    The fleet is moving side to side and collision to a side the whole fleet drops and changes direction

--- settings.py
+++ settings.py
@@ -17,3 +17,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (255, 192, 203)
         self.bullets_allowed = 6
+
+        # Alien settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction fo 1 represents right; -1 represents left
+        self.fleet_direction = 1
