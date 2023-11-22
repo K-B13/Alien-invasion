cc328a31929115e84d370a8f52c9a2b90cd5a54b
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-11-22 21:46:21 +0000

    Set up high score which updates as soon as it is broken

--- alien_invasion.py
+++ alien_invasion.py
@@ -110,9 +110,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            for aliens in collisions.value():
+            for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
@@ -216,7 +217,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
-            self.sb.prep_score
+            self.sb.prep_score()
 
             #Get rid of any remaining aliens and bullets.
             self.aliens.empty()
