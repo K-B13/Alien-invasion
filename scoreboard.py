42c92e44c8e849869b91f7dfaafc2b010722950e
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-11-22 19:23:25 +0000

    Render the scoreboard and fix type in scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -6,12 +6,12 @@ class Scoreboard:
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
         self.screen = ai_game.screen
-        self.screen_react = self.screen.get_rect()
+        self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
         # Font settings for scoring informstion
-        self.text_color = (30, 30, 30)
+        self.text_color = (255, 192, 203)
         self.font = pygame.font.SysFont(None, 48)
 
         # Prepare the initial score image.
